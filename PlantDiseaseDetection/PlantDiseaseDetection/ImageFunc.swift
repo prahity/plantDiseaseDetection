a7fa3f6a673d1c0c2b9589be52cf7bedc5f15a0b
Prahit Yaugand <prahityaugand@gmail.com>
2025-03-01 17:16:43 -0600

    Updated api-key

---
+++ PlantDiseaseDetection/PlantDiseaseDetection/ImageFunc.swift
@@ -0,0 +1,42 @@
+//
+//  ImageFunc.swift
+//  PlantDiseaseDetection
+//
+//  Created by Prahit Yaugand on 3/1/25.
+//
+
+import Foundation
+
+struct ImagePicker: UIViewControllerRepresentable {
+    @Binding var image: UIImage?
+    var sourceType: UIImagePickerController.SourceType
+
+    func makeUIViewController(context: Context) -> UIImagePickerController {
+        let picker = UIImagePickerController()
+        picker.sourceType = sourceType
+        picker.delegate = context.coordinator
+        return picker
+    }
+
+    func updateUIViewController(_ uiViewController: UIImagePickerController, context: Context) {}
+
+    func makeCoordinator() -> Coordinator {
+        return Coordinator(self)
+    }
+
+    class Coordinator: NSObject, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+        let parent: ImagePicker
+
+        init(_ parent: ImagePicker) {
+            self.parent = parent
+        }
+
+        func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+            if let selectedImage = info[.originalImage] as? UIImage {
+                parent.image = selectedImage
+            }
+            picker.dismiss(animated: true)
+        }
+    }
+}
+
